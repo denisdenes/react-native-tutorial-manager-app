89415cea22d1b74f6be0cd81121b6d7e36f9e9c0
Denis Denes <denis.denes@visma.com>
2018-01-29 23:28:01 +0200

    Add error auth message

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -2,6 +2,7 @@ import React, { Component }                         from 'react';
 import { connect }                                  from 'react-redux';
 import { Card, CardSection, Input, Button }         from './common';
 import { emailChanged, passwordChanged, loginUser } from "../actions";
+import { View, Text }                               from 'react-native';
 
 class LoginForm extends Component {
   render() {
@@ -26,6 +27,8 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        {this.renderError()}
+
         <CardSection>
           <Button onPress={this.onButtonPress.bind(this)}>
             Login
@@ -47,12 +50,33 @@ class LoginForm extends Component {
     const { email, password } = this.props;
     this.props.loginUser({ email, password });
   }
+
+  renderError() {
+    if (this.props.error) {
+      return (
+        <View style={{ backgroundColor: 'white' }}>
+          <Text style={styles.errorTextStyle}>
+            {this.props.error}
+          </Text>
+        </View>
+      );
+    }
+  }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 const mapStateToProps = state => {
   return {
     email: state.auth.email,
-    password: state.auth.password
+    password: state.auth.password,
+    error: state.auth.error
   };
 };
 
